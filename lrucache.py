511c2e8a31701f7db0ec2f9b419c767fbe1988e9
Manaswinich <chinthalapudimanaswini.7@msitprogram.net>
2020-04-14 20:40:00 +0530

    test cases modified,code

--- lrucache.py
+++ lrucache.py
@@ -3,8 +3,7 @@ from collections import OrderedDict
 
 class lrucache:
     def __init__(self, size):
-        super().__init__()
-        self.cache = OrderedDict
+        self.cache = OrderedDict()
         self.size = size
 
     def put(self, key, value):
