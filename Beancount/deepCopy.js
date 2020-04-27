6c8050333fe36f705668309a48c5370077abded8
Manaswinich <chinthalapudimanaswini.7@msitprogram.net>
2020-04-27 18:09:54 +0530

    code modified

--- Beancount/deepCopy.js
+++ Beancount/deepCopy.js
@@ -1,7 +1,7 @@
 function deepCopy(obj) {
     const keys = Object.keys(obj)
     const newObj = {}
-    for (let i = 0; i < keys.lenght; i++) {
+    for (let i = 0; i < keys.length; i++) {
         const key = keys[i];
         if (typeof obj[key] === 'object') {
             newObj[key] = deepCopy(obj[key])
